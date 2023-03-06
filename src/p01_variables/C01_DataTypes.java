b17672c1b8a42493b6e6e5d85bf10188a7458f17
Enesy8 <nsmaily8@gmail.com>
2023-03-06 21:39:42 +0300

    v1

---
+++ src/p01_variables/C01_DataTypes.java
@@ -0,0 +1,59 @@
+package p01_variables;
+
+public class C01_DataTypes {
+
+    public static void main(String[] args) {
+
+        /*
+
+              DATA TYPES
+
+                  Primitive Data Types
+                  Non-Primitive Data Types
+
+                      PRIMITIVE DATA TYPES
+
+                          Boolean Type
+                          Numeric Types
+
+                              BOOLEAN TYPE
+
+                                  boolean
+
+                              NUMERIC TYPES
+
+                                  Integral Types
+                                  Character Type
+
+                                      INTEGRAL TYPES
+
+                                          Integer Types
+                                          Floating-Point Types
+
+                                              INTEGER TYPES
+
+                                                  byte
+                                                  short
+                                                  int
+                                                  long
+
+                                              FLOATING-POINT TYPES
+
+                                                  float
+                                                  double
+
+                                      CHARACTER TYPE
+
+                                          char
+
+                      NON-PRIMITIVE DATA TYPES
+
+                          String
+                          Array
+                          ...
+
+        */
+
+    }
+
+}
